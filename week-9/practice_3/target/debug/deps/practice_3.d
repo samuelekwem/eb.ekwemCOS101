86275b3e7c51f14bd5300c540958e6273f7f3003
samuelekwem <samuel.ekwem@pau.edu.ng>
2025-11-24 09:56:00 +0100

    (My eigth commit)

---
+++ week-9/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\HP\eb.ekwemCOS101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\HP\eb.ekwemCOS101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
